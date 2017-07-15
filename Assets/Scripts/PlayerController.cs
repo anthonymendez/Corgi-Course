7261ef08f4f1e875375c591333082849f26af6f3
Anthony Mendez <anthonymendez9@gmail.com>
2017-07-15 14:05:04 -0400

    Set up game environment. Added player controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float jumpPower;
+
+    public bool isGround;
+    Rigidbody2D physicsBody;
+
+	// Use this for initialization
+	void Start () {
+        physicsBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool jumpButton = Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space);
+        if (isGround && jumpButton) {
+            physicsBody.AddForce(Vector3.up * jumpPower * physicsBody.gravityScale * 100f);
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGround = true;
+        }
+    }
+
+    void OnCollisionStay2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision) {
+        if (collision.gameObject.CompareTag("Ground")) {
+            isGround = false;
+        }
+    }
+}
