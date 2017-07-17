97f3db6eaa589d589c541e6315f5aa5865540ad7
Anthony Mendez <anthonymendez9@gmail.com>
2017-07-17 11:44:18 -0400

    Added High Score tracking

---
+++ Assets/Scripts/FinalScoreUI.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FinalScoreUI : MonoBehaviour {
+
+    Text thisText;
+    int highScore;
+
+	// Use this for initialization
+	void Start () {
+        thisText = GetComponent<Text>();
+        thisText.text = "" + GameController.gameScore;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        thisText.text = "" + GameController.gameScore;
+    }
+}
---
+++ Assets/Scripts/HighScoreUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HighScoreUI : MonoBehaviour {
+
+    public Text newHighScore;
+    Text thisText;
+    int highScore;
+
+	// Use this for initialization
+	void Start () {
+        thisText = GetComponent<Text>();
+        highScore = PlayerPrefs.GetInt("HighScore");
+	}
+
+	// Called when enabled
+	void OnEnable () {
+
+        if(GameController.gameScore > highScore) {
+            highScore = GameController.gameScore;
+            thisText.text = highScore + "";
+            PlayerPrefs.SetInt("HighScore",highScore);
+            newHighScore.gameObject.SetActive(true);
+        } else {
+            newHighScore.gameObject.SetActive(false);
+        }
+
+	}
+}
