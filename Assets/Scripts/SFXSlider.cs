15f1566c50611935e4c7a1a4a6f507caab51097a
Anthony Mendez <anthonymendez9@gmail.com>
2017-07-17 00:25:48 -0400

    Sound Effects Slider implemented

--- Assets/Scripts/SFXSlider.cs
+++ Assets/Scripts/SFXSlider.cs
@@ -1,18 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SFXSlider : MonoBehaviour {
 
-    
+    public AudioMixer sfxMixer;
+    Slider sfxSlider;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    void Start() {
+        sfxSlider = GetComponent<Slider>();
+
+        sfxSlider.onValueChanged.AddListener(setVolume);
+    }
+
+    public void setVolume(float SFXLevel) {
+        sfxMixer.SetFloat("SFXVolume",SFXLevel);
+    }
 }
